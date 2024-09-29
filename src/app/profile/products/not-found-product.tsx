50a4930fb254ed24b91f2312b64873629b8e48e7
Farzad <farzade.taherzade@gmail.com>
2024-09-30 02:58:20 +0330

    add not found product vector

--- src/app/profile/products/not-found-product.tsx
+++ src/app/profile/products/not-found-product.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
-import NotFoundProductVector from "@/assets/9276433.jpg"
+import NotFoundProductVector from "@/assets/9276433-removebg-preview.png"
 
 export default function NotFoundProduct() {
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col items-center mt-10'>
+            <h4 className='text-4xl text-primary'>Sorry No Product Founded!</h4>
             <Image
                 src={NotFoundProductVector}
                 height={455} // Desired size with correct aspect ratio
                 width={455} // Desired size with correct aspect ratio
                 alt="not found product"
+                className='bg-transparent'
             />
         </div>
     )
